d680605e0dd5217cfbbfcebe0b73c8c0f77e82ae
Andrew Kudriavtsev <mail.andrew.k@gmail.com>
2019-06-02 14:59:27 +0300

    use nuxt-property-decorator

--- pages/index.vue
+++ pages/index.vue
@@ -18,7 +18,7 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
+import { Vue, Component } from 'nuxt-property-decorator'
 import MyTsClass from '~/assets/js/MyTsClass'
 import MyJsClass from '~/assets/js/MyJsClass'
 
